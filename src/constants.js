546937af9bb551437ab98e14595a27e958e50a87
Cesar Ricardo Lazcano Valdez <la384541@uaeh.edu.mx>
2026-09-04 18:51:45 -0600

    Se crea rama para entrega 3.5.0

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const SITE_INFO={
+    TELEFONO_CURSOS: "+52 1 55 1234 5678",
+    EMAIL_CURSOS: "consultoria@havenser.com",
+    NOMBRE: "Havenser",
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Asegúrate de que esta línea apunte correctamente a tus carpetas y extensiones
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
